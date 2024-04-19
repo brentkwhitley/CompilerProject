21edde357679eed5f7c56b1864e43de5aae8779c
Ben <bennyballin11@gmail.com>
2024-04-19 11:20:42 -0400

    Added genLLCode functions and removed syntax errors

--- CompilerProject/src/main/java/bbs/compilerproject/parser/Declaration.java
+++ CompilerProject/src/main/java/bbs/compilerproject/parser/Declaration.java
@@ -1,8 +1,12 @@
 package bbs.compilerproject.parser;
 
+import bbs.compilerproject.lowlevel.CodeItem;
 import java.io.PrintWriter;
 
 abstract class Declaration {
     
     public abstract void print(PrintWriter pr, int indentation);
+    
+    public abstract CodeItem genLLCode();
+    
 }
